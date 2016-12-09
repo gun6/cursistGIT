e8d4ccd2d2e413893fff896cf6f13c4677b4f1c7
Gunther <gunther_vlerick@hotmail.com>
2016-12-09 15:47:12 +0100

    9 dec 2016 einde dag

--- groenetenen/src/main/java/be/vdab/web/ControllersConfig.java
+++ groenetenen/src/main/java/be/vdab/web/ControllersConfig.java
@@ -1,12 +1,18 @@
 package be.vdab.web;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -33,5 +39,22 @@ public class ControllersConfig extends WebMvcConfigurerAdapter {
 	public void addViewControllers(ViewControllerRegistry registry){
 		registry.addViewController("/info").setViewName("info");
 	}
-
+	
+	@Bean
+	MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+		source.setBasename("classpath:teksten");
+		source.setFallbackToSystemLocale(false);
+		return source;
+	}
+	
+	@Bean
+	LocaleResolver localeResolver() {
+		return new SessionLocaleResolver();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	registry.addInterceptor(new LocaleChangeInterceptor());
+	}
 }
